a0423f0d9960bfa84d46fc9978d8d39c11b15e8c
prajwaldivekar <prajwaldivekar2007@gmail.com>
2024-09-05 06:25:55 +0530

    Naming Convention

--- veriables_in_js/script.js
+++ veriables_in_js/script.js
@@ -3,7 +3,17 @@
 
 var age = 18;
 
+// datatype varname = value ; Strongly Typed language
+//    ex-- java c c++
+// js==> loos language
+
 var num1=12;
 var num2 =8;
 
 console.log(num1 + num2);
+
+// Naming convention
+// firstname
+// first_name
+// FirstName 
+// firstName ==> not like this 1name this is not valid
