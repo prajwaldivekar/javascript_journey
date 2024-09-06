498ddab7830f205a9924d160940b8e5c33a3cf58
prajwaldivekar <prajwaldivekar2007@gmail.com>
2024-09-06 06:35:03 +0530

    AND and OR oprator

---
+++ Logical_oprator/script.js
@@ -0,0 +1,17 @@
+//Logical Oprator
+//AND ==>all the condition must be true
+const age = 20;
+const gender = 'male';
+
+if(age >= 18 && gender == 'male'){
+    console.log("You Are adult male");
+}
+
+//OR
+const date = 2;
+const day = saturday;
+
+if(date = 1 &&  day == saturday){
+    console.log("You Are adult male");
+}
+
