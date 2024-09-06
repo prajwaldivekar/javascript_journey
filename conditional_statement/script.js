26b46b077007d5197fa634de54eb4c13885a9c3c
prajwaldivekar <prajwaldivekar2007@gmail.com>
2024-09-06 05:51:05 +0530

    Learnig ternary opretor

--- conditional_statement/script.js
+++ conditional_statement/script.js
@@ -1,9 +1,14 @@
 // COnditional statement is --> IfElse
 
 const age =21;
-if(age=>18){
+if(age>=18){
     console.log("You Can VIte..!")
 }
 else{
     console.log("You Can't VOte..!")
 }
+console.log(age);
+
+// ternary oprater
+
+age >= 18 ? console.log("Yes") : console.log("Not");
