da26bbbf4b6082167ba5b00cc36ec4ab36a93474
prajwaldivekar <prajwaldivekar2007@gmail.com>
2024-09-07 06:36:26 +0530

    Function Defining

---
+++ Functions/script.js
@@ -0,0 +1,8 @@
+//Functions in javascript
+    // A block of code that perform a perticular task
+
+    function Sayhello(){
+        console.log("Hello..!")   //---> Function Defining
+    }
+
+    Sayhello();   //---> Function Calling
