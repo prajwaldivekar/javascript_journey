c1ac9c3473081cc3400d5c1d7018df511c22076b
prajwaldivekar <prajwaldivekar2007@gmail.com>
2024-09-08 06:41:51 +0530

    change element in array

--- Array/array.js
+++ Array/array.js
@@ -1,3 +1,7 @@
 //Array
-const student =['Prajwal','Advit','Swarup'];
+const student =['Prajwal','Advit','Swarup','joan'];
 console.log(student);
+
+//to change array element
+// array_name[position//[]] = to be change
+student[3]='Aditya';
