c52a9f49325e6e2416fba0d4fbd84a76f3060f42
prajwaldivekar <prajwaldivekar2007@gmail.com>
2025-07-14 19:20:52 +0530

    upgrade file

--- datatypes_and_opretors/script.js
+++ datatypes_and_opretors/script.js
@@ -11,7 +11,43 @@ let num = 3.14;
 let num2 =10;
 
 //Strings
-const name= prajwaldivekar;
+const name= "prajwaldivekar";
 
+//Boolean
+let booleanNum = false;
+console.log(booleanNum);
+
+//  null
+let isloggedIn = null;
+console.log(isloggedIn);
+
+
+
+// Object
+
+let Prajwal ={
+    name: "Prajwal Divekar",
+    age : 18
+
+}
+console.log(Prajwal);
+
+// oprators 
+// adittion , substraction , multiplcation , division
+
+let firstNum = 12;
+let secondNumber = 10;
+
+let add = (firstNum + secondNumber);
+console.log(add);
+
+let sub = (firstNum - secondNumber);
+console.log(sub);
+
+let mul = (firstNum * secondNumber);
+console.log(mul);
+
+let div = (firstNum / secondNumber);
+console.log(div);
 
 
