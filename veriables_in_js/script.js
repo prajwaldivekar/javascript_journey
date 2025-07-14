ac8fa8d4e7675423304f0e38c7da5e4b3aea63b4
prajwaldivekar <prajwaldivekar2007@gmail.com>
2025-07-14 19:54:15 +0530

    Some changes

--- veriables_in_js/script.js
+++ veriables_in_js/script.js
@@ -1,36 +1,56 @@
 //variable
 //var
 
-var age = 18;
+// var age = 18;
 
-// datatype varname = value ; Strongly Typed language
-//    ex-- java c c++
-// js==> loos language
+// datatype varname = value ; 
+// Strongly Typed language ex-- java c c++ in which datatype is strongly check
 
-var num1=12;
-var num2 =8;
+// js==> loos language  javascript is not strongly check language
 
-console.log(num1 + num2);
+// var num1=12;
+// var num2 =8;
+
+// console.log(num1 + num2);
+
+
 
 // Naming convention
+
 // firstname
 // first_name
 // FirstName 
 // firstName ==> not like this 1name this is not valid
 
-// Scope of variables are Global
 
-if(true){
-var age = 23;
-}
- console.log(age);
 
-//  scope of let is local
+// Scope of variables are local
+
+//        Local means declare inside the block or function OR -- Local scope means a variable is accessible only within the block or function where it is declared.
 
 // if(true){
-//     var age = 23;
+//      var age = 23;
 //     }
+//     console.log(age);
+
+    function VAR(){
+         var age = 23;
+         console.log(age);
+
+    }
+    // console.log(VAR);  X it is wrong 
+    VAR();
+    
+
+//  scope of let is Global
+
+// Global means ==> A variable has global scope if it is declared outside of any function, block, or module
+
+// let age = 23;
+//  if(true){
 //      console.log(age);
+//     }
+
 // can't use outside the loop
 
 // const
