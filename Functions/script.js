2666bbb8a18a39d8e4f18f2e1a45dcc721f58b31
prajwaldivekar <prajwaldivekar2007@gmail.com>
2025-07-15 10:00:29 +0530

    functions

--- Functions/script.js
+++ Functions/script.js
@@ -7,18 +7,32 @@ function Sayhello() {
 
 Sayhello()   //---> Function Calling
 
+
+            // argument
 function add(a, b) {
     return a + b;
 }
-add(3, 5)
+add(3, 5)  //Parameters
+
+
 
 function mul(num1, num2) {
     return num1 * num2;
 }
 mul(3, 5)
 
+
+
 function sub(c,d){
     return c - d;
 }
 let result =sub(3,2)
 console.log(`The Substraction is`,result )
+
+
+
+
+function ml(v,b){
+    console.log(v*b);
+}
+ml(1,2);
